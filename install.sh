37dde2ac6844aa62e6d7a2d744a7d14a6df66ac4
Zulfilham <code.zulfilham@gmail.com>
2020-11-02 18:17:56 +0700

    Add a check so that installation occurs only in Termux
    It checks whether /data/data/com.termux/files directory exists or not.
    
    I know this is not the best way to check whether a user uses Termux or
    not.  You could just make the same directory (if you have access), and
    that would make the script thinks that you are using Termux.  But I
    think this will work properly for now, unless you interfere.

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ function slowprint () {
    local string="$1";
    local length=${#string};
 
-   for (( i=0; i<length; i++ )); do
+   for ((i=0; i<length; i++)); do
       echo -n "${string:i:1}";
       sleep 0.010;
    done;
@@ -12,50 +12,46 @@ function slowprint () {
 }
 
 function main () {
-   echo -en "\e[41;1mImportant to note:\e[0m\n";
-   sleep 0.5;
-   echo -en "\e[33m";
-   slowprint "The installation will overwrite your existing configurations ...";
-   sleep 1;
-   slowprint "Only do this if you really know what you're doing.";
-   sleep 2;
-   echo -en "\e[0m";
-   echo -en "\e[3F\e[0J";
+   if [ -d "/data/data/com.termux/files" ]; then
+      echo -en "\e[41;1mImportant to note:\e[0m\n";
+      sleep 0.5;
+      echo -en "\e[33m";
+      slowprint "The installation will overwrite your existing configurations...";
+      sleep 1;
+      slowprint "Only do this if you really know what you're doing.";
+      sleep 2;
+      echo -en "\e[0m\e[3F\e[0J";
 
-   while true; do
-      read -ep $'\e[41;1mImportant to note:\e[0m\n\e[33mThe installation will overwrite your existing configurations ...\nOnly do this if you really know what you\'re doing.\e[0m\n\nContinue anyway (Y/n) > ';
+      while true; do
+         read -ep $'\e[41;1mImportant to note:\e[0m\n\e[33mThe installation will overwrite your existing configurations...\nOnly do this if you really know what you\'re doing.\e[0m\n\nContinue anyway (Y/n) > ';
 
-      case "${REPLY,,}" in
-         y)
-            scriptname="$(realpath "$0")";
-            dirname="$(dirname "$scriptname")";
-
-            if [ -e "/data/data/com.termux/files" ] && [ -d "/data/data/com.termux/files" ]; then
-               echo -en "\e[33minstalling ...\e[0m\n";
-               cp -r "$dirname/home/." $HOME 2> /dev/null;
-               cp -r "$dirname/nano/." "$PREFIX/share/nano" 2> /dev/null;
-               cp -r "$dirname/etc/." "$PREFIX/etc" 2> /dev/null;
+         case "${REPLY,,}" in
+            y)
+               echo -en "\e[33minstalling...\e[0m\n";
+               local dirname="$(dirname "$0")";
+               cp -r "$dirname/home/." $HOME/ 2> /dev/null;
+               cp -r "$dirname/nano/." $PREFIX/share/nano/ 2> /dev/null;
                echo -en "\e[1F\e[0K\e[32mIt's done.\e[0m\n";
                echo -en "\e[97mPlease restart Termux in order the configurations to take effect.\e[0m\n";
-            else
-               echo "Fatal error: Unknown platform, this configuration only valid for \`Termux'." >&2;
-               exit 1;
-            fi;
-            break;
-            ;;
-         n)
-            echo -en "\e[97mAbort.\e[0m\n";
-            break;
-            ;;
-         *)
-            if (( $? == 0 )); then
-               echo -en "\e[5F\e[0J";
-            else
-               echo -en "\e[4F\e[0J";
-            fi;
-            ;;
-      esac;
-   done;
+               return 0;
+               ;;
+            n)
+               echo -en "\e[97mAbort.\e[0m\n";
+               return 1;
+               ;;
+            *)
+               if (($? == 0)); then
+                  echo -en "\e[5F\e[0J";
+               else
+                  echo -en "\e[4F\e[0J";
+               fi;
+               ;;
+         esac;
+      done;
+   else
+      echo "Fatal error: unknown platform, this configuration only valid for Termux." >&2;
+      return 1;
+   fi;
 }
 
 main;
