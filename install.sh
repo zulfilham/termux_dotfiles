d1d2e05ff9d34cb1c6f2a044a5a57e0ea9961ef9
Zulfilham <code.zulfilham@gmail.com>
2021-10-10 15:56:44 +0700

    Fix dependencies installer script
    The old installer script is apparently checking packages by the command
    name, whether a package is installed or not.  That was a wrong check.
    Thus, a package should be checked by its package name instead.  Suppose
    if a package and its command name were different, then the installer
    would install that package every single time.
    
    Although the script checks for a package with the same name as the
    package name does, the Bash built-in command "command -V" treats the
    same name to be a command instead.  As a result, the check always fails
    since there will be no command to be found.
    
    So, to properly install the dependencies, all of the package names will
    be passed to the pkg command as arguments.  Since it performs update and
    install out of the box.  So, no more package checking.

--- install.sh
+++ install.sh
@@ -8,14 +8,8 @@ function main () {
       echo "Fatal: Unknown platform, this configuration only valid for Termux" 1>&2;
       return 1;
    fi;
-
    local FILES="$(dirname "$PREFIX")" DIRNAME="$(dirname "$0")";
-   local required_dependencies=(coreutils termux-am termux-tools procps diffutils nano vim);
-   local installed_dependencies=($(command -V ${required_dependencies[@]} | grep --only-matching "^[a-z0-9-]\+"));
-
-   if [ "${required_dependencies[@]}" != "${installed_dependencies[@]}" ]; then
-      pkg install --assume-yes ${required_dependencies[@]};
-   fi;
+   pkg install --assume-yes coreutils termux-am termux-tools procps diffutils nano vim;
    cp --archive --backup=numbered --target-directory="$FILES" -- "$DIRNAME/home";
    cp --archive --backup=numbered --target-directory="$FILES" -- "$DIRNAME/usr";
 }
