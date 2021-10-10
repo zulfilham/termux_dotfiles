4352eb43cadf8798106b7e4a9ce1868b56f87978
Zulfilham <code.zulfilham@gmail.com>
2021-10-10 14:34:20 +0700

    Refactor installation script
    - Fix the check whether the user uses Termux or not; If the user uses
      Termux, then the output of "uname --operating-system" will be
      "Android", other than that is considered as not Termux
    
    - Make the script to be non-interactive; Perform installation without
      prompt.  Of course do not overwrite an existing file, as
      --backup=numbered option makes a backup of each existing destination
      file

--- install.sh
+++ install.sh
@@ -1,57 +1,23 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
-function slowprint () {
-   local string="$1";
-   local length=${#string};
-
-   for ((i=0; i<length; i++)); do
-      echo -n "${string:i:1}";
-      sleep 0.010;
-   done;
-   echo;
-}
-
 function main () {
-   if [ -d "/data/data/com.termux/files" ]; then
-      echo -en "\e[41;1mImportant to note:\e[0m\n";
-      sleep 0.5;
-      echo -en "\e[33m";
-      slowprint "The installation will overwrite your existing configurations...";
-      sleep 1;
-      slowprint "Only do this if you really know what you're doing.";
-      sleep 2;
-      echo -en "\e[0m\e[3F\e[0J";
-
-      while true; do
-         read -ep $'\e[41;1mImportant to note:\e[0m\n\e[33mThe installation will overwrite your existing configurations...\nOnly do this if you really know what you\'re doing.\e[0m\n\nContinue anyway (Y/n) > ';
-
-         case "${REPLY,,}" in
-            y)
-               echo -en "\e[33minstalling...\e[0m\n";
-               local dirname="$(dirname "$0")";
-               cp -r "$dirname/home/." $HOME/ 2> /dev/null;
-               cp -r "$dirname/nano/." $PREFIX/share/nano/ 2> /dev/null;
-               echo -en "\e[1F\e[0K\e[32mIt's done.\e[0m\n";
-               echo -en "\e[97mPlease restart Termux in order the configurations to take effect.\e[0m\n";
-               return 0;
-               ;;
-            n)
-               echo -en "\e[97mAbort.\e[0m\n";
-               return 1;
-               ;;
-            *)
-               if (($? == 0)); then
-                  echo -en "\e[5F\e[0J";
-               else
-                  echo -en "\e[4F\e[0J";
-               fi;
-               ;;
-         esac;
-      done;
-   else
-      echo "Fatal error: unknown platform, this configuration only valid for Termux." >&2;
+   if (($# > 0)); then
+      echo "Fatal: Expected no arguments, but $# given" 1>&2;
+      return 1;
+   elif [ $(uname --operating-system) != Android ]; then
+      echo "Fatal: Unknown platform, this configuration only valid for Termux" 1>&2;
       return 1;
    fi;
+
+   local FILES="$(dirname "$PREFIX")" DIRNAME="$(dirname "$0")";
+   local required_dependencies=(coreutils termux-am termux-tools procps diffutils nano vim);
+   local installed_dependencies=($(command -V ${required_dependencies[@]} | grep --only-matching "^[a-z0-9-]\+"));
+
+   if [ "${required_dependencies[@]}" != "${installed_dependencies[@]}" ]; then
+      pkg install --assume-yes ${required_dependencies[@]};
+   fi;
+   cp --archive --backup=numbered --target-directory="$FILES" -- "$DIRNAME/home";
+   cp --archive --backup=numbered --target-directory="$FILES" -- "$DIRNAME/usr";
 }
 
-main;
+main "$@";
