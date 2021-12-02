59146da3fb2cc38c521429efc243a3e659124914
Zulfilham <code.zulfilham@gmail.com>
2021-12-03 02:30:32 +0700

    Fix installation issue
    - Make sure to copy sources.list first before installing, so that apt
      knows where to get packages information from
    
    - Warn the user to restart the Termux, otherwise some configuration
      won't take affect

--- install.sh
+++ install.sh
@@ -8,17 +8,20 @@ function main () {
       echo "Fatal: Unknown platform, this configuration only valid for Termux" 1>&2;
       return 1;
    fi;
+
+   # General installation
+   local dirname="$(dirname -- "$0")";
+   cp --archive --backup=numbered -- "$dirname/usr/etc/apt" "$PREFIX/etc";
    apt-get update --assume-yes;
+   apt-get install --assume-yes coreutils diffutils gnupg nano procps termux-am termux-tools;
 
    # Personal installation
    apt-get install atomicparsley bc bzip2 ccrypt clang cronie crunch curl ffmpeg file findutils git grep gzip httrack imagemagick jq lua53 man mariadb openssh p7zip php python rclone renameutils rsync samefile sed sqlite tar termux-api termux-auth termux-elf-cleaner texinfo tree tsu unrar unzip vim vtutils xxhash xz-utils zip && \
    pip install youtube-dl;
 
-   # General installation
-   apt-get install --assume-yes coreutils diffutils gnupg nano procps termux-am termux-tools;
-
    # Main installation
-   cp --archive --backup=numbered -- "$(dirname "$0")"/{home,usr} ~/..;
+   cp --archive --backup=numbered -- "$dirname"/{home,usr} ~/..;
+   echo "Please restart Termux to apply the changes!";
 }
 
 main "$@";
